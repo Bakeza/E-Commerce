d9bd75e2619f6275c8195091cfcf1af1e43f6d21
salsabeelomar <omarsalsabeel65@gmail.com>
2023-05-23 11:25:08 +0300

    user auth

--- client/src/Context/UserContext.js
+++ client/src/Context/UserContext.js
@@ -1,9 +1,27 @@
-import { createContext, useState } from "react";
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
 
 export const UserContext = createContext(null);
 
 export const UserProvider = ({ children }) => {
   const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    const getUserData = async () => {
+      const { data } = await axios.post(
+        `${process.env.REACT_APP_API}/auth/me`,
+        {},
+        {
+          headers: {
+            authorization: `Bearer ${localStorage.getItem("token")}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      setUserData(data.data.user);
+    };
+    getUserData();
+  }, []);
 
   return (
     <UserContext.Provider value={{ userData, setUserData }}>
